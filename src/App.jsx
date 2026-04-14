595c43c1b51581c4f8f0762bfb82c07b91f5b80f
salmansalluz10 <salmansalluzz56@gmail.com>
2026-04-14 15:15:54 +0530

    release-time

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Login from './Pages/Login'
+import Dashboard from './Pages/Dashboard'
+
+function App() {
+
+  return (
+    <>
+     <Routes>
+      <Route path='/' element={<Login/>}/>
+      <Route path='/dashboard' element={<Dashboard/>}/>
+     </Routes>
+    </>
+  )
+}
+
+export default App
